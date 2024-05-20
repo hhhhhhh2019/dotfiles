c8987bf64c85a61a7cc31c26cd67093b86aac760
Alex <agalove543210@gmail.com>
2024-05-20 18:32:54 +0300

    fix strange treesitter bug with tabstop

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,73 +1,72 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		-- name = "nvim-treesitter.configs",
+		name = "nvim-treesitter.configs",
 		dependencies =  {
 			"nvim-treesitter/nvim-treesitter-textobjects",
 			"nvim-treesitter/nvim-treesitter-refactor",
 		},
 
-		init = function ()
-			vim.filetype.add {
-				extension = { rasi = 'rasi' },
-				pattern = {
-					['.*/waybar/config'] = 'jsonc',
-					['.*/mako/config'] = 'dosini',
-					['.*/kitty/*.conf'] = 'bash',
-					['.*/hypr/.*%.conf'] = 'hyprlang',
-				},
-			}
+		-- init = function ()
+			-- vim.filetype.add {
+			-- 	-- extension = { rasi = 'rasi' },
+			-- 	pattern = {
+			-- 		['.*/waybar/config'] = 'jsonc',
+			-- 		['.*/mako/config'] = 'dosini',
+			-- 		['.*/kitty/*.conf'] = 'bash',
+			-- 		['.*/hypr/.*%.conf'] = 'hyprlang',
+			-- 	},
+			-- }
 
-			require("nvim-treesitter.configs").setup({
-				auto_install = true,
-				highlight = {enable = true},
-				autotag = {enable = true},
-				indent = {enable = true},
+		opts = {
+			auto_install = true,
+			highlight = {enable = true},
+			autotag = {enable = true},
+			indent = {enable = true},
 
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true,
-					},
-
-					swap = {
-						enable = true,
-						swap_next = {
-							["<space>a"] = "@parameter.inner",
-						},
-						swap_previous = {
-							["<space>A"] = "@parameter.inner",
-						},
-					},
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
 				},
 
+				swap = {
+					enable = true,
+					swap_next = {
+						["<space>a"] = "@parameter.inner",
+					},
+					swap_previous = {
+						["<space>A"] = "@parameter.inner",
+					},
+				},
+			},
+
+			refactor = {
+				highlight_definitions = {
+					enable = true,
+					clear_on_cursor_move = true,
+				},
+				highlight_current_scope = { enable = true },
+				-- smart_rename = {
+				-- 	enable = true,
+				-- 	keymaps = {
+				-- 		smart_rename = "grr",
+				-- 	},
+				-- },
 				refactor = {
-					highlight_definitions = {
+					navigation = {
 						enable = true,
-						clear_on_cursor_move = true,
-					},
-					highlight_current_scope = { enable = true },
-					-- smart_rename = {
-					-- 	enable = true,
-					-- 	keymaps = {
-					-- 		smart_rename = "grr",
-					-- 	},
-					-- },
-					refactor = {
-						navigation = {
-							enable = true,
-							keymaps = {
-								goto_definition = "gnd",
-								list_definitions = "gnD",
-								list_definitions_toc = "gO",
-								goto_next_usage = "<a-*>",
-								goto_previous_usage = "<a-#>",
-							},
-						}
-					},
+						keymaps = {
+							goto_definition = "gnd",
+							list_definitions = "gnD",
+							list_definitions_toc = "gO",
+							goto_next_usage = "<a-*>",
+							goto_previous_usage = "<a-#>",
+						},
+					}
 				},
-		})
-	end,
+			},
+		},
 	},
 
 	{
