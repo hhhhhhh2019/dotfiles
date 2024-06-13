a0985ae352fe6ce72ecc002ca4c3595fd9e82542
name <mail>
2024-06-13 14:21:05 +0300

    fix

--- .config/kitty/change_theme.sh
+++ .config/kitty/change_theme.sh
@@ -2,10 +2,14 @@
 
 mode=$(dbus-send --session --print-reply=literal --reply-timeout=1000 --dest=org.freedesktop.portal.Desktop /org/freedesktop/portal/desktop org.freedesktop.portal.Settings.Read string:'org.freedesktop.appearance' string:'color-scheme' | tail -c2)
 
-rm -f theme.conf
+CONFIG="$HOME/.config/kitty/"
+
+rm -f "$CONFIG/theme.conf"
 
 if [ "$mode" == "2" ]; then
- ln -s latte.conf theme.conf
+ ln -s "$CONFIG/latte.conf" "$CONFIG/theme.conf"
 else
- ln -s mocha.conf theme.conf
+ ln -s "$CONFIG/mocha.conf" "$CONFIG/theme.conf"
 fi
+
+kitty @ action load_config_file
