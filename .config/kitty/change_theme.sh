c5544ab205afd5cd06d0b0a313641b41e17454e6
Alex <agalove543210@gmail.com>
2024-06-13 19:27:13 +0300

    fix

--- .config/kitty/change_theme.sh
+++ .config/kitty/change_theme.sh
@@ -6,10 +6,10 @@ CONFIG="$HOME/.config/kitty/"
 
 rm -f "$CONFIG/theme.conf"
 
-if [ "$mode" == "2" ]; then
- ln -s "$CONFIG/latte.conf" "$CONFIG/theme.conf"
-else
+if [ "$mode" == "1" ]; then
  ln -s "$CONFIG/mocha.conf" "$CONFIG/theme.conf"
+else
+ ln -s "$CONFIG/latte.conf" "$CONFIG/theme.conf"
 fi
 
 kitty @ action load_config_file
