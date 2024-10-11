4a9726868a2cca545e95752ab43eedd5e4581b06
Alex <agalove543210@gmail.com>
2024-10-11 19:14:37 +0300

    save

--- .config/hypr/resize_window.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for _ in {1..20}; do
-	hyprctl dispatch resizeactive "$1" "$2"
-	sleep 0.001
-done
--- .config/hypr_def/resize_window.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-while true; do
-	hyprctl dispatch resizeactive "$1" "$2"
-done;
