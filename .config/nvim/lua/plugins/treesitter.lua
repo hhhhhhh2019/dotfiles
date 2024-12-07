6e7efc2cec0c730e851614d9f60eaaee320329f8
Alex <agalove543210@gmail.com>
2024-12-07 21:24:14 +0300

    fix treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	opts = {
-		auto_install = true,
-
-		highlight = {
-			enable = true,
-		},
-
-		indent = {
-			enable = true,
-		},
-	}
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			auto_install = true,
+			highlight = {
+				enable = true,
+			},
+			indent = {
+				enable = true
+			},
+		})
+	end
 }
