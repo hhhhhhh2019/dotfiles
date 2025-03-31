c6c586a0f5e5bfb36899298d63388fdf7ee3b0d5
A <a@a.com>
2025-03-31 14:22:08 +0300

    some changes

--- .config/nvim/lua/config/settings.lua
+++ .config/nvim/lua/config/settings.lua
@@ -6,10 +6,10 @@ vim.opt.cursorlineopt = "number"
 vim.opt.clipboard = "unnamedplus"
 
 vim.opt.list = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.expandtab = false
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+-- vim.opt.softtabstop = 2
+-- vim.opt.expandtab = false
 
 vim.opt.undofile = true
 
