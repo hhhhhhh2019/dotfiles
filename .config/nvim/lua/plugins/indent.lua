f2c6e2686e0d96512796ca0ab7b4fb629d7d2aed
A <a@a.com>
2025-07-31 18:15:51 +0300

    neovim indent

--- .config/nvim/lua/plugins/indent.lua
+++ .config/nvim/lua/plugins/indent.lua
@@ -1,4 +1,24 @@
 return {
+{
 	"nmac427/guess-indent.nvim",
+	priority = 100,
 	opts = {}
+},
+{
+	"aruyu/nvim-indentconfig",
+	priority = 101,
+	opts = {
+		default = {
+			expandtab = false,
+			size = 4
+		},
+		exclusions = {
+			{
+				expandtab = true,
+				size = 4,
+				filetype = { "python", "haskell" },
+			},
+		}
+	}
+}
 }
